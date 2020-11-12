e37ffc6efb7105d504e1eff4b50eadf5e0069828
Michelle Jiang <mj3733@students.necsd.net>
2020-11-12 15:22:36 +0000

    I did it

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,15 @@
-
+import java.util.Scanner;
 
 public class Chapter2Test {
   public static void main(String[] args) {
-  
+Scanner scan = new Scanner (System.in);
+
+System.out.println("Please enter two integers:");
+int x = scan.nextInt();
+int y = scan.nextInt();
+        
+System.out.println("The average is: " + ((double)(x+y)/2));
+
+
   }
 }
